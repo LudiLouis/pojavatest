75b253f638c025a7f56d0bba2cd7161bc9380b8a
Wiktor <Wiktor@192.168.0.157>
2021-05-05 13:26:42 +0200

    First Commit

---
+++ TestGit/src/TestGitJava.java
@@ -0,0 +1,40 @@
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class TestGitJava extends JFrame implements ActionListener {
+
+	public TestGitJava() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestGitJava(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestGitJava(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestGitJava(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
